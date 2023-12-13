6523d3278fde1cfbc983febd18eea82bc3ef9f72
renninxD <flores97juanignacio@gmail.com>
2023-12-12 22:22:46 -0300

    preentrega1Floresjs

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+let montoTotal = Number(prompt ("Ingresar Monto"));
+if(montoTotal>=0){
+let cuotas = Number(prompt("Ingrese el numero de cuotas"));
+if (cuotas>=0; cuotas< 25;) {
+    alert(`Sus coutoas a pagar son ${montoTotal / coutas}`) *7.3
+}else{
+    alert("Ingrese un numero de cuotas valido");
+     }
+    }
